0a411d1e0c98a128f3388e8e9de85559eee5141a
unknown <quocdungnguyendo@gmail.com>
2024-03-15 18:00:00 +0700

    txt file for lab 4

---
+++ lab5txt.py
@@ -0,0 +1,20 @@
+# students.txt
+1,Son Ha, 01-01-2003
+2,Duc Hiep,02-12-2003
+3,Xuan Phuong,03-03-2004
+
+# courses.txt
+1,Mathematics
+2,Physics
+3,Chemistry
+
+# marks.txt
+1,1,15
+1,2,16
+1,3,17
+2,1,15
+2,2,13
+2,3,17
+3,1,12
+3,2,14
+3,3,19
